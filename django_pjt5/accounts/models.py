8604449359d5ada67a17eb10fa73ad4e4fbf03cf
dayeong <95da.yeong@gmail.com>
2020-05-20 17:09:00 +0900

    Paginator

--- django_pjt5/accounts/models.py
+++ django_pjt5/accounts/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from movies.models import *
 
 # Create your models here.
 class User(AbstractUser):
     profile_picture = models.ImageField(blank=True)
     followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name = 'followings')
+    like_movies = models.ManyToManyField(Movie, related_name = 'users_like_movie')
+    like_genres = models.ManyToManyField(Genre, related_name = 'users_like_genre')
--- django_pjt5/movies/views.py
+++ django_pjt5/movies/views.py
@@ -3,11 +3,19 @@ from .models import Movie, Genre
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 
+from django.core.paginator import Paginator
+
 # Create your views here.
 def index(request):
     movies = Movie.objects.all()
+    paginator = Paginator(movies, 10)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     context = {
-        'movies':movies,
+        'movies' : movies,
+        'page_obj' : page_obj,
     }
     return render(request, 'movies/index.html', context)
 
