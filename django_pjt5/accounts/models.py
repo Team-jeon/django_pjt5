51b6eaaf9108b4742a10575619e881c880645f23
happpppy <js3891129@gmail.com>
2020-05-20 16:44:47 +0900

    444

--- django_pjt5/accounts/models.py
+++ django_pjt5/accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
-from movies.models import Review
 
 # Create your models here.
 class User(AbstractUser):
